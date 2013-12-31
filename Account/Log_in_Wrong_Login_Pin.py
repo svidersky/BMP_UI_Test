a42682fc3e70cb285c022a088ad14f8974852dcf
Leonid Svidersky <svidersky.leonid@gmail.com>
2013-12-31 16:16:31 +0400

    Little changes

--- Account/Log_in_Wrong_Login_Pin.py
+++ Account/Log_in_Wrong_Login_Pin.py
@@ -24,6 +24,19 @@ class Untitled(unittest.TestCase):
             except: pass
             time.sleep(1)
         else: self.fail("time out")
+        
+        driver.find_element_by_id("login_login").clear()      
+        driver.find_element_by_id("login_login").send_keys("sv40")
+        driver.find_element_by_id("login_password").clear()
+        driver.find_element_by_id("login_password").send_keys("1234")
+        driver.find_element_by_link_text(u"Войти").click()
+        for i in range(20):
+            try:
+                if u"Не верный." == driver.find_element_by_id("login_password_error").text: break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        
      
     def tearDown(self):
         self.driver.quit()
