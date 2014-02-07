d8c768a6b6e61c282dc4b690ce4fa33548b85294
Leonid Svidersky <svidersky.leonid@gmail.com>
2014-02-07 11:31:35 +0400

    RC

--- rebuildAndRun.sh
+++
@@ -1,1 +0,0 @@
-python simpleTest.py
--- test_Log_in_successful.py
+++ test_Log_in_successful.py
@@ -1,36 +1,24 @@
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*- 
 
-from selenium import webdriver
+from selenium import selenium
 import unittest, time, re
 
 class Untitled(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Remote(desired_capabilities=webdriver.DesiredCapabilities.CHROME)
-        self.driver.implicitly_wait(10)
-        self.base_url = "http://buymeapie.com"
         self.verificationErrors = []
-        self.accept_next_alert = True
+        self.selenium = selenium("localhost", 4444, "*chrome", "http://buymeapie.com/")
+        self.selenium.start()
     
-    def test_Log_in_successful(self):
-        driver = self.driver
-        driver.get(self.base_url + "/press")
-        driver.find_element_by_link_text("RU").click()
-        driver.find_element_by_link_text("EN").click()
-        driver.find_element_by_link_text(u"Log in").click()
-        driver.find_element_by_id("login_login").clear()
-        driver.find_element_by_id("login_login").send_keys("sv40")
-        driver.find_element_by_id("login_password").clear()
-        driver.find_element_by_id("login_password").send_keys("1111")
-        driver.find_element_by_css_selector("#login > a.button.blue").click()
-        try: self.assertEqual("sv40", driver.find_element_by_link_text("sv40").text)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        driver.find_element_by_xpath("//div[@id='ng-app']/div[2]/div/div/div/div").click()
-        try: self.assertEqual(u"Список покупок", driver.find_element_by_xpath("//div[@id='ng-app']/div[2]/div/div/div/div").text)
-        except AssertionError as e: self.verificationErrors.append(str(e))
+    def test_untitled(self):
+        sel = self.selenium
+        sel.open("/contacts")
+        sel.click("link=EN")
+        sel.click("link=RU")
+        sel.wait_for_page_to_load("5000")
     
     def tearDown(self):
-        self.driver.quit()
+        self.selenium.stop()
         self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
