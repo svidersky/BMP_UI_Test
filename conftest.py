5b2a0bf96d914c83788581314b5be72919c7c606
Leonid Svidersky <svidersky.leonid@gmail.com>
2015-08-28 12:01:36 +0300

    Разделил модель на мобильную и десктопную

--- conftest.py
+++ conftest.py
@@ -1,11 +1,13 @@
 import pytest
-from model.application import Application
+from model.appplication_mobile import ApplicationMobile
+from model.application_desktop import ApplicationDesktop
 from selenium import webdriver
 
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox", help="browser type")
-    parser.addoption("--base_url", action="store", default="http://buymeapie.com/press", help="base URL")
+    parser.addoption("--base_url", action="store", default="http://buymeapie.com/", help="base URL")
+    parser.addoption("--client_type", action="store", default="desktop", help="client type")
 
 
 @pytest.fixture(scope="session")
@@ -17,9 +19,13 @@ def browser_type(request):
 def base_url(request):
     return request.config.getoption("--base_url")
 
+@pytest.fixture(scope="session")
+def client_type(request):
+    return request.config.getoption("--client_type")
+
 
 @pytest.fixture(scope="session")
-def app(request, browser_type, base_url):
+def app(request, browser_type, base_url, client_type):
     if browser_type == "firefox":
         driver = webdriver.Firefox()
     elif browser_type == "chrome":
@@ -27,4 +33,8 @@ def app(request, browser_type, base_url):
     elif browser_type == "ie":
         driver = webdriver.Ie()
     request.addfinalizer(driver.quit)
-    return Application(driver, base_url)
+    if client_type == "desktop":
+        return ApplicationDesktop(driver, base_url)
+    elif client_type == "mobile":
+        base_url = "http://m.buymeapie.com/"
+        return ApplicationMobile(driver, base_url)
--- test_login_m.py
+++
@@ -1,5 +0,0 @@
-from model.user import User
-
-def test_login_with_valid_credentials(app):
-    app.login_m(User.user_0000())
-    app.login_successful_m(User.user_0000())
