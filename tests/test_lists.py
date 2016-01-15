c5d9b5cc0e716e76baf9e7f76fb2ac0c8519d640
Leonid Svidersky <svidersky.leonid@gmail.com>
2016-01-15 17:49:42 +0300

    Uncommented funcs in test_lists.py

--- tests/test_lists.py
+++ tests/test_lists.py
@@ -13,25 +13,25 @@ def test_prepare(app):
     :return:
     '''
     app.login(User.user_0000())
-    # app.delete_list()
+    app.delete_list()
 
 
-# def test_create_list(app):
-#     '''
-#     Create a new list
-#     :param app:
-#     :return:
-#     '''
-#     app.create_list()
-#
-#
-# def test_rename_list(app):
-#     '''
-#     Rename active list
-#     :param app:
-#     :return:
-#     '''
-#     app.rename_list()
+def test_create_list(app):
+    '''
+    Create a new list
+    :param app:
+    :return:
+    '''
+    app.create_list()
+
+
+def test_rename_list(app):
+    '''
+    Rename active list
+    :param app:
+    :return:
+    '''
+    app.rename_list()
 
 
 def test_add_product(app):
@@ -41,34 +41,34 @@ def test_add_product(app):
     :return:
     '''
     app.add_product(Product.product_test())
-#
-#
-# def test_buy_product(app):
-#     '''
-#     Buy the specified item from a current shopping list
-#     :param app:
-#     :return:
-#     '''
-#     app.buy_product(Product.product_test())
-#
-#
-# #def test_delete_product(app):
-#  #   app.delete_product(Product.product_test())
-#
-#
-# def test_delete_list(app):
-#     '''
-#     Delete active shopping list
-#     :param app:
-#     :return:
-#     '''
-#     app.delete_list()
-#
-#
-# def test_logout(app):
-#     '''
-#     Log out
-#     :param app:
-#     :return:
-#     '''
-#     app.logout()
+
+
+def test_buy_product(app):
+    '''
+    Buy the specified item from a current shopping list
+    :param app:
+    :return:
+    '''
+    app.buy_product(Product.product_test())
+
+
+#def test_delete_product(app):
+#   app.delete_product(Product.product_test())
+
+
+def test_delete_list(app):
+    '''
+    Delete active shopping list
+    :param app:
+    :return:
+    '''
+    app.delete_list()
+
+
+def test_logout(app):
+    '''
+    Log out
+    :param app:
+    :return:
+    '''
+    app.logout()
